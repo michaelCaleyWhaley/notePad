bb27a28ce57cc5c430ee18eb1a26a38f4e490562
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-01-15 23:26:36 +0000

    Adding functionality to store Notes and detect duplicates

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ var parsedArgv = argv.argv;
 
 if (parsedArgv._[0] === "addNote") {
 	notes.addNote(parsedArgv.title, parsedArgv.body);
-} else if (parsedCommand === "deleteNote") {
+} else if (parsedArgv._[0] === "deleteNote") {
 	console.log('note deleted');
-} else if(parsedCommand === "listNote"){
+} else if(parsedArgv._[0] === "listNote"){
 	console.log('listing notes');
-} else if(parsedCommand === "readNote"){
+} else if(parsedArgv._[0] === "readNote"){
 	console.log('reading notes');
 } else {
 	console.log('Command not recognised');
 	console.log('Options include: addNote, deleteNote, listNote, readNote');
+	console.log('Syntax = node app.js addNote --title=Gladiator --body="Some body"');
 }
--- notes.js
+++ notes.js
@@ -1,10 +1,37 @@
-// console.log('notes.js');
-// console.log(process);
 
+const fs = require('fs');
 
 var addNote = (title, body) => {
-	console.log("Title " + title);
-	console.log("Body: " + body);
+	var notes = [];
+	var note = {
+		title: title,
+		body: body
+	};
+
+	// searches for pre existing notes
+	try {
+		var existingNotes = fs.readFileSync('notes-data.json');
+		notes = JSON.parse(existingNotes);
+	} catch(e) {
+		console.log('No existing notes found');
+	}
+
+	// filter runs on each item in the notes array. 
+	// Creating a new array from items that match the condition
+	// in this case any title which matches a pre existing title
+	var duplicateNotes = notes.filter((item) => {
+		return item.title === title;
+	});
+
+	// if duplicate notes is === 0 then the title is original and can be saved
+	if(duplicateNotes.length === 0){
+		notes.push(note);
+		notes = JSON.stringify(notes);
+		fs.writeFileSync('notes-data.json', notes);
+	} else {
+		console.log('A matching title was found');
+	}
+	
 };
 
 module.exports = {
