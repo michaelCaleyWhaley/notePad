73e745e4144594f37246e4885677fbbd24fedaad
michaelCaleyWhaley <michael.caley@river-island.com>
2018-01-18 17:19:52 +0000

    refactoring fetch and save functions

--- notes.js
+++ notes.js
@@ -1,33 +1,35 @@
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+	// searches for pre existing notes
+	try {
+		var existingNotes = fs.readFileSync('notes-data.json');
+		return JSON.parse(existingNotes);
+	} catch (e) {
+		return [];
+	}
+};
+
+var saveNotes = (notes) => {
+	notes = JSON.stringify(notes);
+	fs.writeFileSync('notes-data.json', notes);
+};
+
 var addNote = (title, body) => {
-	var notes = [];
+	var notes = fetchNotes();
 	var note = {
 		title: title,
 		body: body
 	};
-
-	// searches for pre existing notes
-	try {
-		var existingNotes = fs.readFileSync('notes-data.json');
-		notes = JSON.parse(existingNotes);
-	} catch(e) {
-		console.log('No existing notes found');
-	}
-
-	// filter runs on each item in the notes array. 
-	// Creating a new array from items that match the condition
-	// in this case any title which matches a pre existing title
+	// filter runs on each item in the notes array. // Creating a new array from items that match the condition // in this case any title which matches a pre existing title
 	var duplicateNotes = notes.filter((item) => {
 		return item.title === title;
 	});
-
 	// if duplicate notes is === 0 then the title is original and can be saved
-	if(duplicateNotes.length === 0){
+	if (duplicateNotes.length === 0) {
 		notes.push(note);
-		notes = JSON.stringify(notes);
-		fs.writeFileSync('notes-data.json', notes);
+		saveNotes(notes);
 		console.log('Note succesfully saved');
 	} else {
 		console.log('A matching title was found');
@@ -35,36 +37,24 @@ var addNote = (title, body) => {
 };
 
 var deleteNote = (title) => {
-	var existingNotes;
+	var existingNotes = fetchNotes();
 	var noteRemoved = false;
-	// Searches for the note data
-	try {
-		existingNotes = fs.readFileSync('notes-data.json');
-		existingNotes = JSON.parse(existingNotes);
-
-		// loops through the returned array looking for a matching title
-		existingNotes.forEach((element) => {
-
-			if(element.title.toLowerCase() === title.toLowerCase()){
-				var index = existingNotes.indexOf(element);
-				existingNotes.splice(index, 1);
-				var newNotes = JSON.stringify(existingNotes);
-				fs.writeFileSync('notes-data.json', newNotes);
-				console.log('Note succesfully removed');
-				noteRemoved = true;
-			}
-
-		});
-	} catch(e) {
-		console.log('Unable to find any notes');
-	}
-
-	if(noteRemoved === false){
+	// loops through the returned array looking for a matching title
+	existingNotes.forEach((element) => {
+		if (element.title.toLowerCase() === title.toLowerCase()) {
+			var index = existingNotes.indexOf(element);
+			existingNotes.splice(index, 1);
+			saveNotes(existingNotes);
+			console.log('Note succesfully removed');
+			noteRemoved = true;
+		}
+	});
+	if (noteRemoved === false) {
 		console.log('No note matching that title was found');
 	}
-}
+};
 
 module.exports = {
 	addNote: addNote,
 	deleteNote: deleteNote
-}
+};
